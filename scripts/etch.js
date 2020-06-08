c7e650366d797c0a127e5d8d364b0e0590cad3cb
Paul Rignall <paul.rignall@gmail.com>
2020-06-08 16:45:09 +0100

    Allow user to clear the grid

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,17 +1,38 @@
 const divContainer = document.querySelector('#grid-container');
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    divContainer.appendChild(gridSquare);
-}
+let squares;
+
+function removeElements() {
+    while (divContainer.childNodes.length > 0) {
+        divContainer.removeChild(divContainer.lastChild);
+    };
+};
+
+function initialiseGrid(numSquares = 16) {
+
+    for (let i = 0; i < numSquares * numSquares; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        divContainer.appendChild(gridSquare);
+    };
+
+    squares = document.querySelectorAll('.grid-square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', changeHoverStatus)
+    });
+};
 
 function changeHoverStatus(e) {
     e.target.classList.add('hovered');
     e.target.removeEventListener('mouseover', changeHoverStatus);
 }
 
-const squares = document.querySelectorAll('.grid-square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', changeHoverStatus)
-})
+function clearGrid() {
+    removeElements();
+    initialiseGrid();
+};
+
+const clearButton = document.querySelector('#btn-clear');
+clearButton.addEventListener('click', clearGrid);
+
+initialiseGrid();
