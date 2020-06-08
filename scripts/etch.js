6c6b5d80a832afa13fce462725575b2115eae19a
Paul Rignall <paul.rignall@gmail.com>
2020-06-08 22:47:49 +0100

    Added functions for different options

--- scripts/etch.js
+++ scripts/etch.js
@@ -3,12 +3,6 @@ const divContainer = document.querySelector('#grid-container');
 let squares;
 let numSquares;
 
-function removeElements() {
-    while (divContainer.childNodes.length > 0) {
-        divContainer.removeChild(divContainer.lastChild);
-    };
-};
-
 function initialiseGrid() {
 
     if (numSquares == undefined) numSquares = 16;
@@ -27,15 +21,41 @@ function initialiseGrid() {
     });
 };
 
-function changeHoverStatus(e) {
-    e.target.classList.add('hovered');
-    e.target.removeEventListener('mouseover', changeHoverStatus);
-}
+function removeElements() {
+    while (divContainer.childNodes.length > 0) {
+        divContainer.removeChild(divContainer.lastChild);
+    };
+};
 
 function clearGrid() {
     numSquares = prompt('How many squares would you like on each side of the grid?');
     removeElements();
     initialiseGrid();
+};
+
+function changeHoverStatus(e) {
+    e.target.classList.add('hovered');
+    e.target.removeEventListener('mouseover', changeHoverStatus);
+};
+
+function changeHoverColour(e) {
+
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 255);
+    };
+
+    let red = generateRandomNumber();
+    let green = generateRandomNumber();
+    let blue = generateRandomNumber();
+
+    e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue})`;
+};
+
+function changeHoverDarker(e) {
+    e.target.style.backgroundColor = 'rgb(60, 60, 60)';
+    if (e.target.style.opacity != '1') {
+        e.target.style.opacity -= '-0.1'
+    };
 };
 
 const clearButton = document.querySelector('#btn-clear');
